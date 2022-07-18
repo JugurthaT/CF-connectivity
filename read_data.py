6db3aa911144ee820dbd8aa6f6b0642ebbd266bd
root <aimen_r@yahoo.fr>
2022-07-18 08:47:44 -0400

    Mon Jul 18 08:47:44 EDT 2022

--- read_data.py
+++ read_data.py
@@ -16,47 +16,53 @@ env = AppEnv()
 #read all the xsuaa service key values from the env variables
 uaa_service = env.get_service(name='xsuaa-demo')
 #read all the connectivity service key values from the env variables
-conn_service = env.get_service(name='connectivity-demo-lite')
-# read the client ID and secret for the connectivity service
+conn_service = env.get_service(name='instance1')
+## read the client ID and secret for the connectivity service
 conn_sUaaCredentials = conn_service.credentials["clientid"] + ':' + conn_service.credentials["clientsecret"]
-# read the On premise proxy host and on premise proxy port for the connectivity service
+## read the On premise proxy host and on premise proxy port for the connectivity service
 proxy_url = conn_service.credentials["onpremise_proxy_host"] + ':' + conn_service.credentials["onpremise_proxy_port"]
-######################################################################
-##### Step 2: Request a JWT token to access the connectivity service##
-######################################################################
-#create authorization with basic authentication using connectivity credentials as base64 format
+#######################################################################
+####### Step 2: Request a JWT token to access the connectivity service##
+#######################################################################
+##create authorization with basic authentication using connectivity credentials as base64 format
 headers = {'Authorization': 'Basic '+ base64.b64encode(bytes(conn_sUaaCredentials ,'utf-8')).decode(), 'content-type': 'application/x-www-form-urlencoded'}
 #create formdata with client ID and grant type
 formdata = [('client_id', conn_service.credentials["clientid"] ), ('grant_type', 'client_credentials')]
-#call the xsuaa service to retrieve JWT
+##call the xsuaa service to retrieve JWT
 response_conn = requests.post(uaa_service.credentials["url"] + '/oauth/token', data=formdata, headers=headers)
-#convert the response to a proper format which can be reused again
+##convert the response to a proper format which can be reused again
 jwt_conn = response_conn.json()["access_token"]
-######################################################################
-##### Step 3: Make a call to backend system via SAP CC to read data ##
-######################################################################
-#Set up basic auth for SAP System - Username and Password
-onpremise_auth = requests.auth.HTTPBasicAuth('<enter your SAP Username of the on-premise system>' , '<Enter the password>!')
-#Enter the exact path to your OData service in the SAP System using the virtual host:port details mentioned in the SAP Cloud Connector
-url_cc =  'http://vhost.atosorigin-ica.com:8080/sap/opu/odata/sap/Z_TRANSPORTS_CDS/Z_TRANSPORTS?$format=json'
-#create a dict with proxy relevant information
-proxyDict = { 'http' : proxy_url }
-#create a header with authorization to the proxy server with the JWT retrieved in Step 2
-headers = {
-'content-type': 'application/json',
-'Proxy-Authorization': 'Bearer ' + jwt_conn,
-'cache-control': 'no-cache'
-}
-#make a get request using the Virtual ODATA url using the proxy details , header and basic authorization for Onpremise system
-response = requests.get( url_cc, proxies=proxyDict, headers=headers, auth = onpremise_auth)
-#conver the data 
-data_response = json.loads(response.text)
+
+#######################################################################
+###### Step 3: Make a call to backend system via SAP CC to read data ##
+#######################################################################
+##Set up basic auth for SAP System - Username and Password
+#onpremise_auth = requests.auth.HTTPBasicAuth('<enter your SAP Username of the on-premise system>' , '<Enter the password>!')
+##Enter the exact path to your OData service in the SAP System using the virtual host:port details mentioned in the SAP Cloud Connector
+#url_cc =  'http://vhost.atosorigin-ica.com:8080/sap/opu/odata/sap/Z_TRANSPORTS_CDS/Z_TRANSPORTS?$format=json'
+##create a dict with proxy relevant information
+#proxyDict = { 'http' : proxy_url }
+##create a header with authorization to the proxy server with the JWT retrieved in Step 2
+#headers = {
+#'content-type': 'application/json',
+#'Proxy-Authorization': 'Bearer ' + jwt_conn,
+#'cache-control': 'no-cache'
+#}
+##make a get request using the Virtual ODATA url using the proxy details , header and basic authorization for Onpremise system
+#response = requests.get( url_cc, proxies=proxyDict, headers=headers, auth = onpremise_auth)
+##conver the data 
+#data_response = json.loads(response.text)
+##manually added
+
+if(jwt_conn is None):
+  print("no connection")
+  data_response="no connection"
 ######################################################################
 ##### Step 4: Return the data and run the app                       ##
 ######################################################################
 @app.route('/')
 def index():
-  return data_response
+  return jwt_conn
 
 if __name__ == '__main__':
 	if cf_port is None:
